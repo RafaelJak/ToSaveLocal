21bf55612e4c13d110d3ccb5e1eb6631f3dfa94b
Rafael Kosciolek <rafaeljakobkosciolek@googlemail.com>
2021-01-21 09:08:59 +0100

    Initial Commit

---
+++ ToSaveLocal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToSaveLocal
+//
+//  Created by Student on 21.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("The End is the Beginning of the End")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToSaveLocal/ToSaveLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToSaveLocalApp.swift
+//  ToSaveLocal
+//
+//  Created by Student on 21.01.21.
+//
+
+import SwiftUI
+
+@main
+struct ToSaveLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
